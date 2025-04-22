93e2b9194712fa815c7e14841bc32e1baf2227dd
Jamoah4203 <justiceamoah4203@gmail.com>
2025-04-22 21:42:04 +0000

    Updated the Allowed host and installation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['acs-0cah.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 
 # Application definition
